4b3a3d32766da6f1a1d0464e5acaa51ca042217d
Suchit <rajrikhy2@gmail.com>
2014-09-23 22:42:40 -0600

    wtf

--- src/net/austinturner/podcast/GUI/PodcastGUI.java
+++ src/net/austinturner/podcast/GUI/PodcastGUI.java
@@ -3,17 +3,27 @@ package net.austinturner.podcast.GUI;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+
 import net.miginfocom.swing.MigLayout;
+
 import javax.swing.JPanel;
+
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+
 import javax.swing.JTabbedPane;
 import javax.swing.JToolBar;
 import javax.swing.JButton;
 import javax.swing.JSplitPane;
 import javax.swing.JLabel;
 import javax.swing.JInternalFrame;
+
 import java.awt.CardLayout;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 import com.jgoodies.forms.layout.FormLayout;
 import com.jgoodies.forms.layout.ColumnSpec;
 import com.jgoodies.forms.layout.RowSpec;
@@ -27,7 +37,22 @@ public class PodcastGUI {
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-		final String APIKey = args[0];
+		
+		String APIkey = "";
+		
+		try {
+			FileReader fr = new FileReader("api.key");
+			BufferedReader APIreader = new BufferedReader(fr);
+			APIkey = APIreader.readLine();
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		final String APIKey = APIkey;
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
